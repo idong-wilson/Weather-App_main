79bb866ba41ab9d1b37c890fa6841f567cd51692
[Idong Wilson] <[idongwilson@gmail.com]>
2024-06-13 16:15:53 -0400

    updated the weathetr endpoint

--- script.js
+++ script.js
@@ -1,6 +1,80 @@
+// let result = document.getElementById("result");
+// let searchBtn = document.getElementById("search-btn");
+// let cityRef = document.getElementById("city");
+
+// // Function to round up a number to the nearest whole number
+// const roundUp = (number) => {
+//   return Math.ceil(number);
+// };
+
+// // Function to fetch weather details from API and display them
+// let getWeather = () => {
+//   let cityValue = cityRef.value;
+//   // If input field is empty
+//   if (cityValue.length == 0) {
+//     result.innerHTML = `<h3 class="msg">Enter a City Name</h3>`;
+//   }
+//   // If input field is NOT empty
+//   else {
+//     let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=imperial`;
+//     // Clear the input field
+//     cityRef.value = "";
+//     fetch(url)
+//       .then((resp) => resp.json())
+//       // If city name is valid
+//       .then((data) => {
+//         console.log(data);
+//         console.log(data.weather[0].icon);
+//         console.log(data.weather[0].main);
+//         console.log(data.weather[0].description);
+//         console.log(data.name);
+//         console.log(data.main.temp_min);
+//         console.log(data.main.temp_max);
+//         // Extract weather description
+//         const weatherDescription = data.weather[0].description;
+//         // Round up temperature values
+//         const temp = roundUp(data.main.temp);
+//         const tempMin = roundUp(data.main.temp_min);
+//         const tempMax = roundUp(data.main.temp_max);
+//         result.innerHTML = `
+//         <h2>${data.name}</h2>
+//         <h4 class="desc">${weatherDescription}</h4>
+//         <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+//         <h1>${temp}&#176;</h1>
+//         <h4 class="weather">feels like: ${data.main.feels_like.toFixed()}°</h4>
+//         <div class="temp-container">
+//             <div>
+//                 <h4 class="title">min</h4>
+//                 <h4 class="temp">${tempMin}&#176;</h4>
+//             </div>
+//             <div>
+//                 <h4 class="title">max</h4>
+//                 <h4 class="temp">${tempMax}&#176;</h4>
+//             </div>
+//         </div>
+//         `;
+//       })
+//       // If city name is NOT valid
+//       .catch(() => {
+//         result.innerHTML = `<h3 class="msg">Oops! City not found</h3>`;
+//       });
+//   }
+// };
+
+// searchBtn.addEventListener("click", getWeather);
+// window.addEventListener("load", getWeather);
+
+
+
+
+
+
 let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
 let cityRef = document.getElementById("city");
+
+// Replace with your OpenWeatherMap API key
+const API_KEY = `${key}`;
 
 // Function to round up a number to the nearest whole number
 const roundUp = (number) => {
@@ -11,51 +85,50 @@ const roundUp = (number) => {
 let getWeather = () => {
   let cityValue = cityRef.value;
   // If input field is empty
-  if (cityValue.length == 0) {
+  if (cityValue.length === 0) {
     result.innerHTML = `<h3 class="msg">Enter a City Name</h3>`;
-  }
-  // If input field is NOT empty
-  else {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=imperial`;
+  } else {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${API_KEY}&units=imperial`;
     // Clear the input field
     cityRef.value = "";
     fetch(url)
-      .then((resp) => resp.json())
-      // If city name is valid
+      .then((resp) => {
+        if (!resp.ok) {
+          throw new Error("City not found");
+        }
+        return resp.json();
+      })
       .then((data) => {
         console.log(data);
-        console.log(data.weather[0].icon);
-        console.log(data.weather[0].main);
-        console.log(data.weather[0].description);
-        console.log(data.name);
-        console.log(data.main.temp_min);
-        console.log(data.main.temp_max);
+
         // Extract weather description
         const weatherDescription = data.weather[0].description;
         // Round up temperature values
         const temp = roundUp(data.main.temp);
         const tempMin = roundUp(data.main.temp_min);
         const tempMax = roundUp(data.main.temp_max);
+        
+        // Render the weather data including city and country
         result.innerHTML = `
-        <h2>${data.name}</h2>
+        <h2>${data.name}, ${data.sys.country}</h2>
         <h4 class="desc">${weatherDescription}</h4>
         <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
         <h1>${temp}&#176;</h1>
-        <h4 class="weather">feels like: ${data.main.feels_like.toFixed()}°</h4>
+        <h4 class="weather">Feels like: ${data.main.feels_like.toFixed()}&#176;</h4>
         <div class="temp-container">
             <div>
-                <h4 class="title">min</h4>
+                <h4 class="title">Min</h4>
                 <h4 class="temp">${tempMin}&#176;</h4>
             </div>
             <div>
-                <h4 class="title">max</h4>
+                <h4 class="title">Max</h4>
                 <h4 class="temp">${tempMax}&#176;</h4>
             </div>
         </div>
         `;
       })
-      // If city name is NOT valid
-      .catch(() => {
+      .catch((error) => {
+        console.error(error);
         result.innerHTML = `<h3 class="msg">Oops! City not found</h3>`;
       });
   }
